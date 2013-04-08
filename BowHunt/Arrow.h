dbf5208d8890958c4d3fed7f94484915d113e350
Cody Creager <pi.rubiks@gmail.com>
2013-04-08 14:19:35 -0400

    Initial commit. Added Arrow class to contain all the information about a given arrow.

---
+++ BowHunt/Arrow.h
@@ -0,0 +1,13 @@
+//
+//  Arrow.h
+//  BowHunt
+//
+//  Created by Cody Creager on 4/8/13.
+//  Copyright (c) 2013 CS330. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Arrow : NSObject
+
+@end
---
+++ BowHunt/Arrow.m
@@ -0,0 +1,13 @@
+//
+//  Arrow.m
+//  BowHunt
+//
+//  Created by Cody Creager on 4/8/13.
+//  Copyright (c) 2013 CS330. All rights reserved.
+//
+
+#import "Arrow.h"
+
+@implementation Arrow
+
+@end
